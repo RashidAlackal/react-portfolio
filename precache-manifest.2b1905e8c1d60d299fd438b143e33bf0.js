f2764e37e702a43fa4eaece218ea629dedb7405e
RashidAlackal <asrashid4@gmail.com>
2020-05-27 12:33:03 +0530

    Updates

---
+++ precache-manifest.2b1905e8c1d60d299fd438b143e33bf0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be47816cffec7fdc17448f4798335b98",
+    "url": "/RashidAlackal/react-portfolio/index.html"
+  },
+  {
+    "revision": "db111d1c9b4ddd362ee7",
+    "url": "/RashidAlackal/react-portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4db4c97906af41083e31",
+    "url": "/RashidAlackal/react-portfolio/static/css/main.92166397.chunk.css"
+  },
+  {
+    "revision": "db111d1c9b4ddd362ee7",
+    "url": "/RashidAlackal/react-portfolio/static/js/2.5250dd0c.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/RashidAlackal/react-portfolio/static/js/2.5250dd0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4db4c97906af41083e31",
+    "url": "/RashidAlackal/react-portfolio/static/js/main.658ed86c.chunk.js"
+  },
+  {
+    "revision": "2c144a8f1a4a5b90f7b5",
+    "url": "/RashidAlackal/react-portfolio/static/js/runtime-main.c1d8a751.js"
+  },
+  {
+    "revision": "3082fdd4aae7d323c65244c069f97736",
+    "url": "/RashidAlackal/react-portfolio/static/media/cv_html-css.3082fdd4.png"
+  },
+  {
+    "revision": "fe23680ab2e1c88e13257005ac4b0e26",
+    "url": "/RashidAlackal/react-portfolio/static/media/portfolio-react.fe23680a.png"
+  },
+  {
+    "revision": "7a5dfe313009cb56265de3db54aba7b6",
+    "url": "/RashidAlackal/react-portfolio/static/media/shopping-cart-html-css.7a5dfe31.png"
+  }
+]);
